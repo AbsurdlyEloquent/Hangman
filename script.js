2e130ee01dce65188e48455fb452afecfac8e77e
Ethan Henderson <ethan.henderson397@gmail.com>
2020-08-19 11:47:35 -0400

    word displays properly

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let wordP = document.querySelector('#word')
 let letters = document.querySelector('#letters')
 let form = document.querySelector('form')
+let theme = document.querySelector('#theme')
+let data;
 
 //Random word fetched from api
 let query = prompt('Enter a theme word')
 fetch(`https://api.datamuse.com/words?ml=${query}&max=4`)
   .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(err => console.error(err))
+  .then(raw => {
+    data = raw
+    splitStr()
+  })
+  .catch(err => console.error(err));
+
+//splits the word into an array and displays hidden characters
+function splitStr(i=0) {
+  let wordArr = data[i].word.split("")
+  wordP.innerText = "_ ".repeat(wordArr.length)
+  theme.innerText = theme.innerText + " " + query
+}
