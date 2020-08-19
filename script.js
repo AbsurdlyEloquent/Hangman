3445e36d2e5acd16447af097ca373e146ed6814f
Ethan Henderson <ethan.henderson397@gmail.com>
2020-08-19 11:20:28 -0400

    wrote fetch statement

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let wordP = document.querySelector('#word')
 let letters = document.querySelector('#letters')
 let form = document.querySelector('form')
+
+//Random word fetched from api
+let query = prompt('Enter a theme word')
+fetch(`https://api.datamuse.com/words?ml=${query}&max=4`)
+  .then(response => response.json())
+  .then(data => data.split())
+  .catch(err => console.error(err);)
