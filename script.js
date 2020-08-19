3d3d9b27a94155d579dc1c28f99e88ec91e7455f
Ethan Henderson <ethan.henderson397@gmail.com>
2020-08-19 13:07:33 -0400

    letters replace properly

--- script.js
+++ script.js
@@ -2,21 +2,48 @@ let wordP = document.querySelector('#word')
 let letters = document.querySelector('#letters')
 let form = document.querySelector('form')
 let theme = document.querySelector('#theme')
-let data;
+let wordArr = []
+let data = []
 
 //Random word fetched from api
 let query = prompt('Enter a theme word')
-fetch(`https://api.datamuse.com/words?ml=${query}&max=4`)
-  .then(response => response.json())
-  .then(raw => {
-    data = raw
-    splitStr()
-  })
-  .catch(err => console.error(err));
-
+fetchWords()
+function fetchWords() {
+  fetch(`https://api.datamuse.com/words?ml=${query}&max=1`)
+    .then(response => response.json())
+    .then(raw => {
+      console.log(raw)
+      if (raw.length > 0) {
+        data = raw
+        splitStr()
+    } else {
+        query = prompt("That word returned no results!")
+        fetchWords()
+    }})
+    .catch(err => console.error(err));
+}
 //splits the word into an array and displays hidden characters
-function splitStr(i=0) {
-  let wordArr = data[i].word.split("")
-  wordP.innerText = "_ ".repeat(wordArr.length)
+function splitStr() {
+  wordArr = data[0].word.split("")
+  wordP.innerText = "_".repeat(wordArr.length)
   theme.innerText = theme.innerText + " " + query
+}
+
+//adds event listener to the form
+form.addEventListener('submit', handler)
+function handler(e) {
+  e.preventDefault()
+  wordArr.forEach((item, i) => {
+    if (wordArr[i] === e.target[0].value.toLowerCase()) {
+      console.log(wordP.innerText)
+      wordP.innerText = replaceAt(wordP.innerText, i, wordArr[i])
+    } else {
+      return false
+    }
+  });
+}
+
+// utility function to change strings, this is used a lot
+replaceAt = function(str, index, replacement) {
+    return str.substr(0, index) + replacement + str.substr(index + replacement.length);
 }
